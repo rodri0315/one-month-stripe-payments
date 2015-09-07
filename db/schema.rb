0d9c0a305b3dfeb3f7adb03603b5a74e9a5858df
Jorge <rodri0315@gmail.com>
2015-09-07 16:04:58 -0400

    Added uuid to purchase object for more secure receipt URL

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904190250) do
+ActiveRecord::Schema.define(version: 20150907195359) do
 
   create_table "purchases", force: :cascade do |t|
     t.string   "email"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150904190250) do
     t.string   "product_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "uuid"
   end
 
 end
